16601bbe76595bfe4ebce02d5675aa36c3f812db
DaniilZinovyev <44101039+DaniilZinovyev@users.noreply.github.com>
2020-02-19 10:17:20 +0200

    Add files via upload
    Second version , which I am not fond of.

--- timeline.js
+++ timeline.js
@@ -2,110 +2,45 @@
 
   // Start
   window.addEventListener("load", tmInit);
-  window.addEventListener("resize", tmInit);  
+  window.addEventListener("resize", tmInit);
 
   function tmInit() {
+    const tm = document.querySelector(".tm");
+    if (!tm) return;
+    const tmRow = tm.querySelector(".tm-inner");
+    const tmItems = tm.querySelectorAll(".tm-item");
 
-    // Vars
-    const tm = document.querySelector(".tm"),
-          row = tm.querySelector(".tm-inner"),
-          tmItems = tm.querySelectorAll(".tm-item");
+    let requiredWidth = getTotalWidth(tmRow, tmItems);
 
-    if ( window.innerWidth > 768 ) {   // On Desktop version
-      setRowWidth(row, tmItems);
-      setRowPadding(tm, tmItems);
-      arrangeItems(tmItems);
-    } else {   // On Mobile version
-      clearStyles(tm, row, tmItems);
+    setView(tm, requiredWidth);
+  }
+
+  function setView(tm, requiredWidth) {
+    if ( tm.offsetWidth < requiredWidth ) {
+
+      // Change to mobile view
+      if (tm.classList.contains('js-mobile')) return;
+
+      tm.classList.add('js-mobile');
+    } else {
+
+      // Change to desktop view
+      if (!tm.classList.contains('js-mobile')) return;
+
+      tm.classList.remove('js-mobile');
     }
   }
 
+  function getTotalWidth(row, items) {
+    let totalWidth = 0;
+    let offset = 0; // Offset between row & any item
 
-  // Set width for .tm-inner based on width of items
-  function setRowWidth(row, items) {
+    // Get margin left value (absolute, numeric)
+    offset = Math.abs(parseInt(getComputedStyle(items[0]).marginLeft));
 
-    let offset = 10,   // Offset between items and row (.tm-inner)
-        summaryWidth = 0;   // Width of all items in top row
-
-    // Is items number odd or even
-    let parity = 0;   
-
-    // If even - parity = 1, if odd - parity = 0
-    items.length % 2 ? parity = 0 : parity = 1;   
-
-    // Calculate summary width of top items (parity = 0) or of bottom items (parity = 1)
-    for (let i = parity; i < items.length; i+=2) {
-      summaryWidth += items[i].offsetWidth + offset;
-    }
-
-    // Remove last offset
-    summaryWidth -= offset; 
-
-    // Set width for row (.tm-inner)
-    row.style.width = summaryWidth + "px";
-  }
-
-
-  // Set padding for .tm elem based on height of items & offset
-  function setRowPadding(tm, items) {
-
-    let offset = 16, // Offset between line & items
-        paddingTop = 0,
-        paddingBottom = 0,
-        highest; // Index of highest element
-
-    // Get highest item between top items (odd .tm-items)
-    highest = 0;
-    for (let i = 0; i < items.length; i+=2) {  
-      if (items[highest].offsetHeight < items[i].offsetHeight) {
-        highest = i;
-      }
-    }
-    paddingTop = items[highest].offsetHeight + offset;
-
-    // Get highest item between bottom items (even .tm-items)
-    highest = 1;
-    for (let i = 1; i < items.length; i+=2) {  
-      if (items[highest].offsetHeight < items[i].offsetHeight) {
-        highest = i;
-      }
-    }
-    paddingBottom = items[highest].offsetHeight + offset;
-
-    // Set styles for .tm
-    tm.style.paddingTop = paddingTop + "px";
-    tm.style.paddingBottom = paddingBottom + "px";
-  }
-
-  // Arrange items
-  function arrangeItems(elems) {
-
-    let elemWidth = elems[0].offsetWidth,         // Elem width (all elems have equal width)
-        offset = 10,                              // Offset between items
-        coefficient = (elemWidth + offset) / 2;   // Value to increase left property
-
-    // For each element ...
-    for (let i = 0; i < elems.length; i++) {
-
-      // Set left offset for each element, based on its index
-      elems[i].style.left = (i * coefficient) + 'px';
-    }
-  }
-
-  // Clear styles written with js ( For mobile version )
-  function clearStyles(tm, row, items) {
-
-    // Clear style attribute for .tm
-    tm.setAttribute("style", "");
-
-    // Clear style attribute for .tm-inner
-    row.setAttribute("style", "");
-
-    // Clear style for each item
-    for (let i = 0; i < items.length; i++) {
-      items[i].setAttribute("style", "");
-    }
-
+    // All items have equal width - important!
+    totalWidth = (items[0].offsetWidth * 2) + (offset * 2);
+    return totalWidth;
   }
   
 })();
